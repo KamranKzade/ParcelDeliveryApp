67b71164ab64389b17470163435e7c1b4892a220
Kamran <311.kamran@gmail.com>
2023-09-04 11:13:30 +0400

    Add project files.

---
+++ AuthServer.API/Models/UserApp.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthServer.API.Models;
+
+public class UserApp : IdentityUser
+{
+	public string? Name { get; set; }
+	public string? Surname { get; set; }
+	public string? Address { get; set; }
+	public string? PostalCode { get; set; }
+	public DateTime Birhtdate { get; set; }
+}
