965743f52dc595fe93d52f9019a23944288202fa
Kamran <311.kamran@gmail.com>
2023-09-14 11:38:16 +0400

    DeliveryServer.API -- > Program.cs Generate

---
+++ DeliveryService.API/Extentions/StartUpExtention.cs
@@ -0,0 +1,67 @@
+﻿using RabbitMQ.Client;
+using SharedLibrary.Models;
+using SharedLibrary.Extentions;
+using DeliveryServer.API.Models;
+using SharedLibrary.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Services.Abstract;
+using SharedLibrary.UnitOfWork.Abstract;
+using SharedLibrary.Repositories.Abstract;
+using DeliveryServer.API.Services.Abstract;
+using DeliveryServer.API.Services.Concrete;
+using SharedLibrary.Services.RabbitMqCustom;
+
+namespace DeliveryServer.API.Extentions;
+
+public static class StartUpExtention
+{
+	public static void AddScopeWithExtention(this IServiceCollection services)
+	{
+
+		services.AddScoped<IUnitOfWork, UnitOfWork.Concrete.UnitOfWork>();
+		services.AddScoped(typeof(IGenericRepository<>), typeof(Repositories.Concrete.GenericRepository<>));
+		services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>));
+		services.AddScoped<IDeliveryService, DeliveryService>();
+	}
+
+	public static void AddDbContextWithExtention(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDbContext<AppDbContext>(opts =>
+		{
+			opts.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+		});
+	}
+
+	public static void AddSingletonWithExtention(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddSingleton(sp => new ConnectionFactory
+		{
+			Uri = new Uri(configuration.GetConnectionString("RabbitMQ")),
+			DispatchConsumersAsync = true
+		});
+		services.AddSingleton<RabbitMQPublisher<OrderDelivery>>();
+		services.AddSingleton<RabbitMQClientService>();
+	}
+
+	public static void AddCustomTokenAuthWithExtention(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.Configure<CustomTokenOption>(configuration.GetSection("TokenOptions"));
+		var tokenOptions = configuration.GetSection("TokenOptions").Get<CustomTokenOption>();
+		services.AddCustomTokenAuth(tokenOptions);
+	}
+
+	public static void AddHttpClientWithExtention(this IServiceCollection services, IConfiguration configuration)
+	{
+		// AuthService -in methoduna müraciet 
+		services.AddHttpClient("OrderService", client =>
+		{
+			client.BaseAddress = new Uri(configuration["OrderServiceBaseUrl"]);
+		});
+	}
+
+	public static void OtherAdditionWithExtention(this IServiceCollection services)
+	{
+		services.UseCustomValidationResponse();
+		services.AddAuthorization();
+	}
+}
--- DeliveryService.API/Program.cs
+++ DeliveryService.API/Program.cs
@@ -1,17 +1,4 @@
-﻿using RabbitMQ.Client;
-using SharedLibrary.Extentions;
-using DeliveryServer.API.Models;
-using SharedLibrary.Configuration;
-using Microsoft.EntityFrameworkCore;
-using SharedLibrary.Services.Abstract;
-using SharedLibrary.UnitOfWork.Abstract;
-using SharedLibrary.Repositories.Abstract;
-using DeliveryServer.API.Services.Abstract;
-using DeliveryServer.API.Services.Concrete;
-using DeliveryServer.API.UnitOfWork.Concrete;
-using DeliveryServer.API.Repositories.Concrete;
-using SharedLibrary.Services.RabbitMqCustom;
-using SharedLibrary.Models;
+﻿using DeliveryServer.API.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,36 +10,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-builder.Services.AddDbContext<AppDbContext>(opts =>
-{
-	opts.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
-});
+builder.Services.AddDbContextWithExtention(builder.Configuration);
+builder.Services.AddScopeWithExtention();
+builder.Services.AddSingletonWithExtention(builder.Configuration);
+builder.Services.AddCustomTokenAuthWithExtention(builder.Configuration);
+builder.Services.OtherAdditionWithExtention();
+builder.Services.AddHttpClientWithExtention(builder.Configuration);
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>));
-builder.Services.AddScoped<IDeliveryService, DeliveryService>();
-
-builder.Services.AddSingleton(sp => new ConnectionFactory
-{
-	Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQ")),
-	DispatchConsumersAsync = true
-});
-builder.Services.AddSingleton<RabbitMQPublisher<OrderDelivery>>();
-builder.Services.AddSingleton<RabbitMQClientService>();
-
-
-builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOptions"));
-var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOption>();
-builder.Services.AddCustomTokenAuth(tokenOptions);
-builder.Services.UseCustomValidationResponse();
-builder.Services.AddAuthorization();
-
-// AuthService -in methoduna müraciet 
-builder.Services.AddHttpClient("OrderService", client =>
-{
-	client.BaseAddress = new Uri(builder.Configuration["OrderServiceBaseUrl"]);
-});
 
 
 var app = builder.Build();
